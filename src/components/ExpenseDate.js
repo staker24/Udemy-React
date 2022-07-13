ce22a5e38c2ac2385c4a92dedca274d203f9dab2
Shahrukh <you@example.com>
2022-07-13 11:42:45 -0500

    date styling

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,19 @@
+//rendering date of an expense
+import "./ExpenseDate.css"
+
+function ExpenseDate(props){
+    const month = props.date.toLocaleString("en-us", { month: "long" });
+    const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+    const year = props.date.getFullYear();
+
+    return (
+      <div class="expense-date">
+        <div class="expense-date__month"> {month} </div>
+        <div class="expense-date__year"> {year} </div>
+        <div class="expense-date__day"> {day} </div>
+      </div>
+    );
+}
+
+
+export default ExpenseDate;
