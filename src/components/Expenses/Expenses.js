4f4c11722c819d3f394aceeb8ced5463656dc24e
Shahrukh223 <57192850+Shahrukh223@users.noreply.github.com>
2022-07-23 01:40:44 -0500

    update

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,38 +13,16 @@ const Expenses = (props) => {
     console.log(filter);
     setChangedFilter(filter);
 
-    }
+  }
     return (
       <div>
         <Card className="expenses">
           <ExpenseFilter selected={changedFilter} onChangeFilter={filterChangeHandler} />
-          <ExpenseItem
-            title={props.obj[0].title}
-            amount={props.obj[0].amount}
-            date={props.obj[0].date}
-          ></ExpenseItem>
-
-          <ExpenseItem
-            title={props.obj[1].title}
-            amount={props.obj[1].amount}
-            date={props.obj[1].date}
-          ></ExpenseItem>
-
-          <ExpenseItem
-            title={props.obj[2].title}
-            amount={props.obj[2].amount}
-            date={props.obj[2].date}
-          ></ExpenseItem>
-
-          <ExpenseItem
-            title={props.obj[3].title}
-            amount={props.obj[3].amount}
-            date={props.obj[3].date}
-          ></ExpenseItem>
+          {props.obj.map(expense => < ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)}
+          
         </Card>
       </div>
     );
-
 }
 
 export default Expenses
